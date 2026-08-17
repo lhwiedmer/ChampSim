685217447b29c34d17a213b5eb0ecb863b47b032
lhmw-PCBUILD1 <lhwiedmer111@gmail.com>
2026-08-17 14:18:04 -0300

    Add classification experiment script

--- classification_experiment.sh
+++ classification_experiment.sh
@@ -1,3 +1,5 @@
-git submodule update --init
-vcpkg/bootstrap-vcpkg.sh
-vcpkg/vcpkg install
+mkdir -p traces/
+awk 'NF{print $NF}' download_traces.txt | parallel --bar -j 4 "curl -o traces/{} -C - --retry 3 https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/{}"
+
+cd ../
+ls traces/*.champsimtrace.xz | parallel --bar -j 49 './bin/champsim_goldencove_1c_hbm_llc --warmup-instructions 200000000 --simulation-instructions 300000000 --ramulator-stats results/ramulator/{/.}.yaml --json results/champsim/{/.}.json {}'
