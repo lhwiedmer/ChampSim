a234a67405b4fd220fb1fedc8999c8336a067bca
lhmw-PCBUILD1 <lhwiedmer111@gmail.com>
2026-08-18 21:40:30 -0300

    Change classification experiment

--- classification_experiment.sh
+++ classification_experiment.sh
@@ -1,5 +1,7 @@
-mkdir -p traces/
-awk 'NF{print $NF}' download_traces.txt | parallel --bar -j 4 "curl -o traces/{} -C - --retry 3 https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/{}"
+#mkdir -p traces/
+#awk 'NF{print $NF}' download_traces.txt | parallel --bar -j 4 "curl -o traces/{} -C - --retry 3 https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/{}"
 
-cd ../
-ls traces/*.champsimtrace.xz | parallel --bar -j 49 './bin/champsim_goldencove_1c_hbm_llc --warmup-instructions 200000000 --simulation-instructions 300000000 --ramulator-stats results/ramulator/{/.}.yaml --json results/champsim/{/.}.json {}'
+#cd ../
+
+mkdir -p results/ramulator results/champsim
+ls traces/ | parallel --bar -j 49 './bin/champsim_goldencove_1c_ddr5_llc --warmup-instructions 200000000 --ramulator-stats results/ramulator/{/.}.yaml --json results/champsim/{/.}.json traces/{}'
